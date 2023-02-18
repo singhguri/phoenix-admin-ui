b4e254239b2271caefab2ef2b09e132cda741138
singhguri <singhguri5598@gmail.com>
2023-02-18 16:54:11 -0500

    added services

--- src/components/taskList.jsx
+++ src/components/taskList.jsx
@@ -1,40 +1,78 @@
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useEffect, useState } from "react";
+import { deleteTaskById, getAllTasks } from "../services/taskService";
 
 const TaskList = (props) => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    getAllTasks()
+      .then((res) => {
+        console.log({ res });
+        if (res.data.status) setTasks(res.data.message);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const handleEdit = (id) => {};
+
+  const handleDelete = async (id) => {
+    await deleteTaskById(id)
+      .then((res) => {
+        if (res.data.status) {
+          setTasks(tasks.filter((task) => task._id !== id));
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
-    
-    
     <div>
       <h1>Task List</h1>
-      <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">No.</th>
-      <th scope="col">Task</th>
-      <th scope="col">Type Of Task</th>
-      <th scope="col">Level Of Task</th>
-      <th scope="col">Action</th>
-    </tr>
-  </thead>
-  <tbody class="table-group-divider">
-    <tr>
-      <th scope="row">1</th>
-      <td>task1</td>
-      <td>online</td>
-      <td>offline</td>
-      <td><button type="button" class="btn btn-info btn-sm"><FontAwesomeIcon icon="fa-regular fa-pen-to-square" />Edit </button>
-      <button type="button" class="btn btn-danger btn-sm"><FontAwesomeIcon icon="fa-solid fa-trash" /> Delete</button>
-      </td>
-    </tr>
-   
-  </tbody>
-  </table>
-  <div class="d-grid gap-2 d-md-block">
-  <button class="btn btn-primary btn-sm" type="button">Create Task</button>
-</div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">No.</th>
+            <th scope="col">Task</th>
+            <th scope="col">Task Type</th>
+            <th scope="col">Task Timing (in min)</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody className="table-group-divider">
+          {tasks.map((task, index) => (
+            <tr key={task._id}>
+              <th>{index + 1}</th>
+              <td>{task.taskName}</td>
+              <td>{task.taskType}</td>
+              <td>{task.taskTiming}</td>
+              <td>
+                <button
+                  type="button"
+                  onClick={() => handleEdit(task._id)}
+                  className="btn btn-info btn-sm me-2"
+                >
+                  <i className="fa fa-pencil me-2" />
+                  Edit
+                </button>
+                <button
+                  type="button"
+                  onClick={() => handleDelete(task._id)}
+                  className="btn btn-danger btn-sm"
+                >
+                  <i className="fa fa-trash me-2" />
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="d-grid gap-2 d-md-block">
+        <button className="btn btn-primary btn-sm" type="button">
+          Create Task
+        </button>
+      </div>
     </div>
-    
   );
 };
 
