63a6178bd13e044ef2440cca32a164e6c40db8b3
ApurvaPatel29 <apurvapatel6136@gmail.com>
2023-02-17 22:15:08 -0500

    added dashboard and tasklist html

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -2,19 +2,68 @@ const Dashboard = (props) => {
   return (
     <div>
       <h1>Dashboard</h1>
-      <div className="row">
-        <div className="col-md-6">
-          <div className="card">
-            <div className="card-body"># of Users</div>
-          </div>
-        </div>
-        <div className="col-md-6">
-          <div className="card">
-            <div className="card-body"># of Tasks</div>
-          </div>
-        </div>
-      </div>
-    </div>
+      <div class="row row-cols-1 row-cols-md-2 g-5">
+      <div class="card text-bg-primary mb-3 " >
+  <div class="card-header">Total User</div>
+  <div class="card-body">
+    <h5 class="card-title">User Number</h5>
+    <p class="card-text">100</p>
+  </div>
+</div>
+<div class="card text-bg-secondary mb-3" >
+  <div class="card-header">Online User</div>
+  <div class="card-body">
+    <h5 class="card-title">Current User Logged In:</h5>
+    <p class="card-text">100</p>
+  </div>
+</div>
+<div class="card text-bg-success mb-3" >
+  <div class="card-header">In App Currency</div>
+  <div class="card-body">
+    <h5 class="card-title">Total Coins purchased</h5>
+    <p class="card-text">100</p>
+  </div>
+</div>
+<div class="card text-bg-danger mb-3" >
+  <div class="card-header">In App Currency</div>
+  <div class="card-body">
+    <h5 class="card-title">Total Coins Spent</h5>
+    <p class="card-text">100</p>
+  </div>
+</div>
+<div class="card text-bg-warning mb-3" >
+  <div class="card-header">Tasks</div>
+  <div class="card-body">
+    <h5 class="card-title">Total Tasks Created</h5>
+    <p class="card-text">100</p>
+  </div>
+</div>
+<div class="card text-bg-info mb-3" >
+  <div class="card-header">Online Task</div>
+  <div class="card-body">
+    <h5 class="card-title">Task which can be play online</h5>
+    <p class="card-text">100</p>
+  </div>
+</div>
+<div class="card text-bg-light mb-3" >
+  <div class="card-header">Offline Task</div>
+  <div class="card-body">
+    <h5 class="card-title">Task which can be play offline</h5>
+    <p class="card-text">100</p>
+  </div>
+</div>
+<div class="card text-bg-dark mb-3" >
+  <div class="card-header">Tasks Level</div>
+  <div class="card-body">
+    <h5 class="card-title">Levels Of Task</h5>
+    <p class="card-text">No. Of Easy task: 100 <br/>
+    No. Of Medium task: 100 <br/>
+    No. Of Hard task: 100 <br/></p>
+  </div>
+</div>
+   </div> 
+   </div>  
+      
   );
 };
 
--- src/components/taskList.jsx
+++ src/components/taskList.jsx
@@ -1,8 +1,40 @@
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 const TaskList = (props) => {
   return (
+    
+    
     <div>
       <h1>Task List</h1>
+      <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">No.</th>
+      <th scope="col">Task</th>
+      <th scope="col">Type Of Task</th>
+      <th scope="col">Level Of Task</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody class="table-group-divider">
+    <tr>
+      <th scope="row">1</th>
+      <td>task1</td>
+      <td>online</td>
+      <td>offline</td>
+      <td><button type="button" class="btn btn-info btn-sm"><FontAwesomeIcon icon="fa-regular fa-pen-to-square" />Edit </button>
+      <button type="button" class="btn btn-danger btn-sm"><FontAwesomeIcon icon="fa-solid fa-trash" /> Delete</button>
+      </td>
+    </tr>
+   
+  </tbody>
+  </table>
+  <div class="d-grid gap-2 d-md-block">
+  <button class="btn btn-primary btn-sm" type="button">Create Task</button>
+</div>
     </div>
+    
   );
 };
 
