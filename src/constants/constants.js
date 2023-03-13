fc929146e77bc0f5d78be884516d1fb4411fdf52
singhguri <singhguri5598@gmail.com>
2023-03-12 23:03:05 -0400

    heroku production config

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:5001/api";
-// export const BASE_URL = "https://phoenix-rest-api.herokuapp.com/api";
+// export const BASE_URL = "http://localhost:5001/api";
+export const BASE_URL = "https://phoenix-rest-api.herokuapp.com/api";
