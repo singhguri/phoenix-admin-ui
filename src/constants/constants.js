9d981028e63658609df9b2324f7a8d8753909b97
singhguri <singhguri5598@gmail.com>
2023-03-29 18:18:17 -0400

    prod url fix

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://localhost:5001/api";
-// export const BASE_URL = "https://phoenix-rest-api.herokuapp.com/api";
+// export const BASE_URL = "http://localhost:5001/api";
+export const BASE_URL = "https://phoenix-rest-api.herokuapp.com/api";
 
 export const Roles = {
   ADMIN: 1,
